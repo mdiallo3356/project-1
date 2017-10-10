3a8756a3818382da524060fffa28d1b67cbac5ed
Steve <Steve@Steven>
2017-10-10 16:50:43 -0400

    Extra change..?

--- Method Madness/src/SFongLib.java
+++ Method Madness/src/SFongLib.java
@@ -3,31 +3,33 @@ public class SFongLib
 {
 	public static void main(String[] args)
 	{
-		System.out.println(quadraticSolver(1,-4,4));
+		System.out.println(quadraticSolver(1,0,-4));
 		System.out.println(sumUpTo(100));
 		System.out.println(cutOut("Hello" , "Hi"));
 		System.out.println(isPalindrome("racecar"));
 	}
-	public static double quadraticSolver(double a, double b, double c)
+	public static String quadraticSolver(double a, double b, double c)
 	{
-		double x = 0;
-		while(x >= 0)
-		{
-			double y= (a*x*x) + (b*x) + c;
-			if(y != 0)
-			{
-				x++;
-			}
-			if(y == 0)
-			{
-				break;
-			}
-		}
-		return x;
+		double x1;
+		x1 = (-b + Math.sqrt((b*b)-4*a*c)) / (2*a);
+		double x2;
+		x2 = (-b - Math.sqrt((b*b)-4*a*c)) / (2*a);
+		String ans = "The roots of " + a + "x^2 + " + b + "x + " + c + " = 0, " + "are " + x1 + ", " + x2 + ".";
+		return ans;
 	}
 	public static int sumUpTo(int x)
 	{
-		return (x*(x + 1)) / 2 ;
+		//return (x*(x + 1)) / 2 ;
+		int sum;
+		if(x == 1)
+		{
+			sum = 1;
+		}
+		else
+		{
+			sum = x + sumUpTo(x - 1);
+		}
+		return sum;
 	}
 	public static String cutOut(String mainStr, String subStr)
 	{
